1b8a30ac1e6f748777b69ea621b475635d6c81e0
David Kuder <davidk@thewaffleiron.net>
2023-04-25 16:15:20 -0400

    Fix for AGS register corruption

--- common/abus.h
+++ common/abus.h
@@ -3,9 +3,9 @@
 void abus_init();
 
 #define CARD_SELECT   ((value & (1u << CONFIG_PIN_APPLEBUS_DEVSEL-CONFIG_PIN_APPLEBUS_DATA_BASE)) == 0)
-#define CARD_DEVSEL   ((address & 0xcf80) == 0xc080)
-#define CARD_IOSEL    (((address & 0xcf00) >= 0xc100) && ((address & 0xcf00) < 0xc700))
-#define CARD_IOSTROBE ((address & 0xc800) == 0xc800)
+#define CARD_DEVSEL   ((address & 0xff80) == 0xc080)
+#define CARD_IOSEL    (((address & 0xff00) >= 0xc100) && ((address & 0xff00) < 0xc700))
+#define CARD_IOSTROBE ((address & 0xf800) == 0xc800)
 
 enum {
     ABUS_MAIN_SM = 0,
