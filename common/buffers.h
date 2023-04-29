ea4af57302ce45ef4e11cf9b088004edbdf172ce
David Kuder <davidk@thewaffleiron.net>
2023-04-28 23:18:51 -0400

    Update buffers.h

--- common/buffers.h
+++ common/buffers.h
@@ -68,45 +68,51 @@ extern volatile uint32_t soft_switches;
 
 extern volatile uint32_t internal_flags;
 
-#define SOFTSW_TEXT_MODE      0x00000001
-#define SOFTSW_MIX_MODE       0x00000002
-#define SOFTSW_HIRES_MODE     0x00000004
-#define SOFTSW_MODE_MASK      0x00000007
-#define SOFTSW_PAGE_2         0x00000008
+#define SOFTSW_TEXT_MODE      0x00000001ul
+#define SOFTSW_MIX_MODE       0x00000002ul
+#define SOFTSW_HIRES_MODE     0x00000004ul
+#define SOFTSW_MODE_MASK      0x00000007ul
+#define SOFTSW_PAGE_2         0x00000008ul
 
 // Apple IIe/c/gs softswitches
-#define SOFTSW_80STORE        0x00000100
-#define SOFTSW_AUX_READ       0x00000200
-#define SOFTSW_AUX_WRITE      0x00000400
-#define SOFTSW_AUXZP          0x00000800
-#define SOFTSW_SLOT3ROM       0x00001000
-#define SOFTSW_80COL          0x00002000
-#define SOFTSW_ALTCHAR        0x00004000
-#define SOFTSW_DGR            0x00008000
+#define SOFTSW_80STORE        0x00000100ul
+#define SOFTSW_AUX_READ       0x00000200ul
+#define SOFTSW_AUX_WRITE      0x00000400ul
+#define SOFTSW_AUXZP          0x00000800ul
+#define SOFTSW_SLOT3ROM       0x00001000ul
+#define SOFTSW_80COL          0x00002000ul
+#define SOFTSW_ALTCHAR        0x00004000ul
+#define SOFTSW_DGR            0x00008000ul
 
-#define SOFTSW_NEWVID_MASK    0xE0
+#define SOFTSW_NEWVID_MASK    0xE0ul
 #define SOFTSW_NEWVID_SHIFT   11
 
-#define SOFTSW_MONOCHROME     0x00010000
-#define SOFTSW_LINEARIZE      0x00020000
-#define SOFTSW_SHR            0x00040000
+#define SOFTSW_MONOCHROME     0x00010000ul
+#define SOFTSW_LINEARIZE      0x00020000ul
+#define SOFTSW_SHR            0x00040000ul
 
-#define SOFTSW_IOUDIS         0x00080000
+#define SOFTSW_IOUDIS         0x00080000ul
 
-#define SOFTSW_SHADOW_MASK    0x7F
+#define SOFTSW_SHADOW_MASK    0x7Ful
 #define SOFTSW_SHADOW_SHIFT   20
 
-#define SOFTSW_SHADOW_TEXT    0x00100000
-#define SOFTSW_SHADOW_HGR1    0x00200000
-#define SOFTSW_SHADOW_HGR2    0x00400000
-#define SOFTSW_SHADOW_SHR     0x00800000
-#define SOFTSW_SHADOW_AUXHGR  0x01000000
-#define SOFTSW_SHADOW_ALTDISP 0x02000000
-#define SOFTSW_SHADOW_IO      0x04000000
+#define SOFTSW_SHADOW_TEXT    0x00100000ul
+#define SOFTSW_SHADOW_HGR1    0x00200000ul
+#define SOFTSW_SHADOW_HGR2    0x00400000ul
+#define SOFTSW_SHADOW_SHR     0x00800000ul
+#define SOFTSW_SHADOW_AUXHGR  0x01000000ul
+#define SOFTSW_SHADOW_ALTDISP 0x02000000ul
+#define SOFTSW_SHADOW_IO      0x04000000ul
 
 // V2 Analog specific softswitches
-#define IFLAGS_OLDCOLOR       0x08000000
-#define SOFTSW_TERMINAL       0x10000000
-#define IFLAGS_TEST           0x20000000
-#define IFLAGS_IIE_REGS       0x40000000
-#define IFLAGS_IIGS_REGS      0x80000000
+#define IFLAGS_VIDEO7         0x04000000ul
+#define IFLAGS_OLDCOLOR       0x08000000ul
+#define IFLAGS_TERMINAL       0x10000000ul
+#define IFLAGS_TEST           0x20000000ul
+#define IFLAGS_IIE_REGS       0x40000000ul
+#define IFLAGS_IIGS_REGS      0x80000000ul
+
+#define IFLAGS_V7_MODE0       0x00000000ul
+#define IFLAGS_V7_MODE1       0x00000001ul
+#define IFLAGS_V7_MODE2       0x00000002ul
+#define IFLAGS_V7_MODE3       0x00000003ul
